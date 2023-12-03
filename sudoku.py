cf07864d47d6d1b66038a2107af4ab0b33d958c4
Your Name <you@example.com>
2023-12-03 17:23:20 +0300

    done

---
+++ sudoku.py
@@ -0,0 +1,55 @@
+def print_board(board):
+    for i in range(9):
+        if i % 3 == 0 and i != 0:
+            print("- - - - - - - - - - - - ")
+
+        for j in range(9):
+            if j % 3 == 0 and j != 0:
+                print("|", end=" ")
+
+            if j == 8:
+                print(board[i][j])
+            else:
+                print(str(board[i][j]) + " ", end="")
+
+def is_valid_move(board, row, col, num):
+    # Kontrol etme işlemleri
+    for i in range(9):
+        if board[row][i] == num or board[i][col] == num or board[3 * (row // 3) + i // 3][3 * (col // 3) + i % 3] == num:
+            return False
+    return True
+
+def solve_sudoku(board):
+    for i in range(9):
+        for j in range(9):
+            if board[i][j] == 0:
+                for num in range(1, 10):
+                    if is_valid_move(board, i, j, num):
+                        board[i][j] = num
+                        if solve_sudoku(board):
+                            return True
+                        board[i][j] = 0
+                return False
+    return True
+
+# Sudoku tahtası örneği
+example_board = [
+    [5, 3, 0, 0, 7, 0, 0, 0, 0],
+    [6, 0, 0, 1, 9, 5, 0, 0, 0],
+    [0, 9, 8, 0, 0, 0, 0, 6, 0],
+    [8, 0, 0, 0, 6, 0, 0, 0, 3],
+    [4, 0, 0, 8, 0, 3, 0, 0, 1],
+    [7, 0, 0, 0, 2, 0, 0, 0, 6],
+    [0, 6, 0, 0, 0, 0, 2, 8, 0],
+    [0, 0, 0, 4, 1, 9, 0, 0, 5],
+    [0, 0, 0, 0, 8, 0, 0, 7, 9]
+]
+
+print("Sudoku Tahtası:")
+print_board(example_board)
+
+if solve_sudoku(example_board):
+    print("\nÇözülmüş Sudoku:")
+    print_board(example_board)
+else:
+    print("\nBu Sudoku çözülemez.")
